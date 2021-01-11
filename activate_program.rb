ab8d65aa11d6c999b5c383cae395b484ddfe7bae
Derek Shiller <dcshiller@gmail.com>
2021-01-11 08:58:39 -0500

    Change pid to wid

--- activate_program.rb
+++ activate_program.rb
@@ -1,26 +1,29 @@
+#! /bin/ruby
+
 require 'yaml'
-FILE_PATH = "./.program_pid_list.yml"
+require 'byebug'
+FILE_PATH = "#{File.expand_path(File.dirname(__FILE__))}/.program_wid_list.yml"
 
 def activate_program(program_name, program_command)
-  program_pid = find_if_open program_name
-  if program_pid
-    bring_to_top program_pid
+  program_wid = find_if_open program_name
+  if program_wid
+    bring_to_top program_wid
   else
-    program_pid = execute program_command
-    store_pid program_name, program_pid
+    program_wid = execute program_command
+    store_wid program_name, program_wid
   end
 end
 
 def find_if_open(program_name)
-  program_pid = find_in_file program_name
-  return nil unless program_pid
-  is_active = verify_active program_pid
-  puts is_active
-  if is_active then program_pid else nil end
+  program_wid = find_in_file program_name
+  return nil unless program_wid
+  is_active = verify_active program_wid
+  if is_active then program_wid else nil end
 end
 
-def verify_active(program_pid)
-  system("ps -p #{program_pid}")
+def verify_active(program_wid)
+  r = `wmctrl -l | awk '$1==\"#{program_wid}\" {print \"true\"}'`.chomp
+  r == 'true'
 end
 
 def find_in_file(program_name)
@@ -33,24 +36,24 @@ def find_in_file(program_name)
   end
 end
 
-def store_pid(program_name, program_pid)
+def store_wid(program_name, program_wid)
   if File.exist? FILE_PATH
     file = File.read(FILE_PATH)
     yml = YAML.load(File.read FILE_PATH)
   else
     yml = {}
   end
-  yml[program_name] = program_pid
+  yml[program_name] = program_wid
   File.write(FILE_PATH, yml.to_yaml)
 end
 
-def bring_to_top(program_pid)
-  r = `wmctrl -lp | awk '$3==#{program_pid} {print $1; exit}'`.chomp
-  `wmctrl -ia #{r}`
+def bring_to_top(program_wid)
+  `wmctrl -ia #{program_wid}`
 end
 
 def execute(program_command)
-  `#{program_command} > /dev/null & echo $!`.chomp
+  `#{program_command} > /dev/null`.chomp
+  `wmctrl -l | awk '{print $1}'`.split("\n").last
 end
 
 activate_program ARGV[0], ARGV[1]
